ff8061237c18b387cba5b12ede58ae26d26da13f
ssarcandy <ssarcandy@gmail.com>
2017-02-17 20:29:36 +0800

    fix bug

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ request(option)
     .then(buffer => {
         const decoded = iconv.decode(new Buffer(buffer), 'big5');
         const table = table2json.convert(decoded)[1];
-        const status = table.length ? '<span style="color:#f00">Down</span>' : '<span style="color:#0f0">Operational</span>';
+        const status = table.length == 1 ? '<span style="color:#f00">Down</span>' : '<span style="color:#0f0">Operational</span>';
         return fs.writeFile(config.output_path + '/index.html', '<div style="font-size:28px; margin: 10px auto;width:500px;"><span>CMLab Mail Server Status: </span>' + status + '<div style="font-size: 14px;margin: 20px;">Update: ' + new Date() + '</div></div>');
     })
     .then(() => console.log('Refresh done.'))
